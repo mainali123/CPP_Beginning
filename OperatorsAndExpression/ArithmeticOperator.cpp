686e24ed2a4718bd600feebbf9082c7bac3cbd1e
Diwash Mainali <diwashmainali900@gmail.com>
2023-07-24 06:03:44 +0545

    Operator and Expression

---
+++ OperatorsAndExpression/ArithmeticOperator.cpp
@@ -0,0 +1,24 @@
+//
+// Created by diwash on 24/07/2023.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 5;
+
+    // compound assignment operator +=, -=, *=, /=. %=
+    a += 7;
+
+
+    cout << "Arithmetic Operators" << endl;
+    cout << (a + b) << endl;
+    cout << (a - b) << endl;
+    cout << (a * b) << endl;
+    cout << (a / b) << endl;
+    cout << (a % b) << endl;
+
+    return 0;
+}
