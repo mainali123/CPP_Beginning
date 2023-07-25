c3d87ba8bf493172c83c0aebc0b347ecd0c80c9e
Diwash Mainali <diwashmainali900@gmail.com>
2023-07-25 05:49:01 +0545

    Star Pattern Problem

---
+++ FlowControl/StarPattern.cpp
@@ -0,0 +1,23 @@
+//
+// Created by diwash on 25/07/2023.
+//
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num;
+    cin>>num;
+
+    for (int i = 1; i <= num; ++i) {
+        // spaces
+        for (int j = 1; j <= num - i; ++j) {
+            cout<<" ";
+        }
+        for (int j = 1; j <= (2 * i) - 1; ++j) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
