34a64ae7766bcf44c5d2cede21208210df8cc239
Diwash Mainali <diwashmainali900@gmail.com>
2023-08-02 19:38:35 +0545

    Coding Exercise-5 K Rotate

---
+++ DSA_Essentials_Part2/CodingExercises/CE-5-K_Rotate.cpp
@@ -0,0 +1,44 @@
+/*
+K-Rotate
+Given an integer vector and a value k, your task is to rotate the array k times clockwise.
+Input Format
+In the function an integer vector and number k is passed.
+Output Format
+Return an integer vector.
+Sample Input
+{1, 3, 5, 7, 9}, x = 2
+Sample Output
+{7, 9, 1, 3, 5}
+
+Explanation
+After 1st rotation - {9, 1, 3, 5, 7}
+After 2nd rotation - {7, 9, 1, 3, 5}*/
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<int> kRotate(vector<int> a, int k) {
+    // your code  goes here
+    int size = a.size();
+    int iteration = k % size;
+
+    vector<int> ans;
+    for (int i = size - iteration; i < size; ++i) {
+        ans.push_back(a[i]);
+    }
+    for (int i = 0; i < size - iteration; ++i) {
+        ans.push_back(a[i]);
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> arr = {1, 3, 5, 7, 9};
+    int target = 2;
+    for (int x: kRotate(arr, target)) {
+        cout << x;
+    }
+    return 0;
+}
